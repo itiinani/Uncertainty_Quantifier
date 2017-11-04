74be5f955153a0f9f4ee339d4ff5f41a51a9d6ae
aksinghal5590 <aksinghal@cs.stonybrook.edu>
2017-11-04 18:31:31 -0400

    Corrected eq_classes.txt parsing routine

--- src/ParseEQ_Class.py
+++ src/ParseEQ_Class.py
@@ -1,70 +1,69 @@
 from src import FaultyTranscriptFilter
 
+
 def parseEQClass():
     trCount = 0
-    eqCount = 0
     lineCount = 0
     trMap = dict()
     trEqMap = dict()
     i = 0
     j = 0
-    unique_list = []
-    amb_map = dict()
     for line in open('../input/eq_classes.txt'):
         lineCount += 1
         if(lineCount == 1):
             trCount = int(line)
             continue
         if(lineCount == 2):
-            eqCount = int(line)
             continue
         if(i < trCount):
             trMap[i] = line[:-1]
             i += 1
             continue
         val = line.split("\t")
-        if val[0] == '1':
-            unique_list.append((trMap[int(val[1])],int(val[2][:-1])))
-        else:
-            for tr_id in val[1:-1]:
-                if trMap[int(tr_id)] in amb_map.keys():
-                    amb_map[trMap[int(tr_id)]] += int(val[len(val)-1][:-1])
+        for tr_id in val[1:-1]:
+            eq_tuple = []
+            if trMap[int(tr_id)] in trEqMap.keys():
+                eq_tuple = trEqMap[trMap[int(tr_id)]]
+                val1 = eq_tuple[0] + int(val[len(val) - 1][:-1])
+                val2 = eq_tuple[1] + 1
+                val3 = eq_tuple[2] + int(val[0])
+                eq_tuple[0] = val1
+                eq_tuple[1] = val2
+                eq_tuple[2] = val3
+            else:
+                eq_tuple.append(int(val[len(val) - 1][:-1]))
+                eq_tuple.append(1)
+                eq_tuple.append(int(val[0]))
+            trEqMap[trMap[int(tr_id)]] = eq_tuple
+    return trEqMap
+
+
+def getUniqueAndAmbiguousMaps():
+    unique_map = dict()
+    uni_uni_map = dict()
+    ambiguous_map = dict()
+    notInEq = []
+    faultyList = FaultyTranscriptFilter.filterFaultyTranscripts()
+    trEQMap = parseEQClass()
+    for tr in faultyList:
+        if tr in trEQMap.keys():
+            eq_tuple = trEQMap[tr]
+            if eq_tuple[1] == 1:
+                if eq_tuple[2] == 1:
+                    uni_uni_map[tr] = eq_tuple[0]
                 else:
-                    amb_map[trMap[int(tr_id)]] = int(val[len(val)-1][:-1])
+                    unique_map[tr] = eq_tuple[0]
+            else:
+                ambiguous_map[tr] = eq_tuple[0]
+        else:
+            notInEq.append(tr)
 
-    return [unique_list, amb_map]
-
-
-faulty = FaultyTranscriptFilter.filterFaultyTranscripts()
-print(len(faulty))
-
-eq_output = parseEQClass()
-
-def compareUniqueTrWithFaulty():
-    unique_list = eq_output[0]
-    uni_common_list = []
-    print(len(unique_list))
-    for tr in unique_list:
-        if tr[0] in faulty:
-            uni_common_list.append(tr)
-    return uni_common_list
+    print("EQ list length: ", len(trEQMap))
+    print("Faulty list length: ", len(faultyList))
+    print("Uni uni mapped faulty list length: ", len(uni_uni_map))
+    print("Uniquely mapped faulty list length: ", len(unique_map))
+    print("Ambiguously mapped faulty list length: ", len(ambiguous_map))
+    print("Non-mapped faulty list length: ", len(notInEq))
 
 
-def compareAmbTrWithFaulty():
-    amb_map = eq_output[1]
-    amb_common_list = []
-    print(len(amb_map))
-    for tr in faulty:
-        if tr in amb_map.keys():
-            amb_common_list.append((tr, amb_map[tr]))
-    return amb_common_list
-
-
-uni_common_list = compareUniqueTrWithFaulty()
-amb_common_list = compareAmbTrWithFaulty()
-
-print(len(uni_common_list))
-print(uni_common_list)
-
-print(len(amb_common_list))
-print(amb_common_list)
+getUniqueAndAmbiguousMaps()
