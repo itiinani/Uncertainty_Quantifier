b95d37830c0fd4c0972f94f385bbcd16ad9b205a
Iti Inani <iti.inani@stonybrook.edu>
2017-11-05 16:07:33 -0500

    Faulty Transcripts Data

--- src/getDataForFaulty.py
+++ src/getDataForFaulty.py
@@ -1,5 +1,29 @@
 import pandas as pd
 
+def getErrorFractionAll():
+    lineCount = 0
+    errorMap = dict()
+    for line in open('../input/errorFraction_all.csv'):
+        lineCount += 1
+        if lineCount == 1:
+            continue
+        data = line.split(',')
+        print(len(data))
+        errorMap[data[1]] = data[2]
+    return errorMap
+
+def getErrorFractionFaulty():
+    lineCount=0
+    errorMap = dict()
+    for line in open('../input/errorFraction_faulty.csv'):
+        lineCount+=1
+        if lineCount ==1:
+            continue
+        data = line.split(',')
+        print(len(data))
+        errorMap[data[1]] = data[2]
+    return errorMap
+
 def createFaultyTranscriptCSV():
     lineCount =0
     faultyMap = dict()
@@ -31,8 +55,9 @@ def createPCData():
     faultyMap = dict()
     download_dir = "../input/PCData.csv"
     csv = open(download_dir, "w")
-    columnTitleRow = "Length,EffectiveLength,TPM,NumReads,Weight\n"
+    columnTitleRow = "NumReads,TPM,ErrorFraction,Weight\n"
     csv.write(columnTitleRow)
+    errorMap = getErrorFractionFaulty()
     for line in open('../input/quant_new.csv'):
         lineCount += 1
         if lineCount == 1:
@@ -42,15 +67,49 @@ def createPCData():
         print(line)
         data = line.split('\t')
         print(len(data))
+
         if data[7] == '"1"\n':
-            length = data[1]
-            effectiveLength = data[2]
             tpm = data[3]
             numReads = data[4]
             weight = data[6]
-            row = length + "," + effectiveLength + "," + tpm + "," + numReads + "," + weight + "\n"
-            csv.write(row)
+            if data[0].replace('"','') in errorMap.keys():
+                errorFraction = errorMap[data[0].replace('"','')]
+                row = numReads + "," + tpm + "," + errorFraction + "," + weight + "\n"
+                csv.write(row)
 
+
+def createDataForForceDirected():
+    trCount = 0
+    lineCount = 0
+    trMap = dict()
+    trEqMap = dict()
+    i = 0
+    j = 0
+    classID =0;
+    download_dir = "../input/ForceDirectedData.csv"
+    csv = open(download_dir, "w")
+    columnTitleRow = "source,target\n"
+    csv.write(columnTitleRow)
+    for line in open('../input/eq_classes.txt'):
+        lineCount += 1
+        if (lineCount == 1):
+            trCount = int(line)
+            continue
+        if (lineCount == 2):
+            continue
+        if (i < trCount):
+            trMap[i] = line[:-1]
+            i += 1
+            continue
+        val = line.split("\t")
+        for tr_id in val[1:-1]:
+            transcript = trMap[int(tr_id)]
+            classNumber = str(classID)
+            row = transcript+","+classNumber+"\n"
+            csv.write(row)
+        classID = classID+1
+
+createDataForForceDirected()
 createFaultyTranscriptCSV()
 createPCData()
 
