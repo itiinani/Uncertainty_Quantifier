4ca49d620dba62bcc62e974417be6199d07ab61b
Iti Inani <iti.inani@stonybrook.edu>
2017-11-05 01:07:32 -0400

    Fixed Errors

--- src/dataForGraphs.py
+++ src/dataForGraphs.py
@@ -1,9 +1,10 @@
 from src import FaultyTranscriptFilter
 import pandas as pd
 
-def getErrorCSV():
+def get_FaultyTranscriptError_CSV():
     faultyList = FaultyTranscriptFilter.filterFaultyTranscripts()
     lineCount = 0
+    lineCount1 = 0
     truthMap = dict()
     quantMap = dict()
     for line in open('../input/poly_truth.tsv'):
@@ -14,22 +15,58 @@ def getErrorCSV():
         if data[0] in faultyList:
             truthMap[data[0]] = int(data[1])
     for line in open('../input/quant.tsv'):
-        lineCount += 1
-        if lineCount == 1:
+        lineCount1 += 1
+        if lineCount1 == 1:
             continue
         data1 = line.split('\t')
         if data1[0] in faultyList:
-            quantMap[data1[0]] = int(data1[1])
+            value = data1[4]
+            numReads = float(value[:-1])
+            quantMap[data1[0]] = numReads
 
     resultMap = dict()
     for x in truthMap.keys():
         truthValue = truthMap.get(x)
         quantValue = quantMap.get(x)
+        if(truthValue is None or quantValue is None):
+            continue
         errorFraction = (truthValue - quantValue)/truthValue
         resultMap[x] = errorFraction
-    df = pd.DataFrame(resultMap.items(), columns=['TranscriptID', 'ErrorFraction'])
-    df.to_csv("errorFraction.csv", encoding='utf-8')
+    df = pd.DataFrame(list(resultMap.items()), columns=['TranscriptID', 'ErrorFraction'])
+    df.to_csv("errorFraction_faulty.csv", encoding='utf-8')
 
-getErrorCSV()
+def get_AllTrancriptsError_CSV():
+    lineCount2 = 0
+    lineCount3 = 0
+    truthMap = dict()
+    quantMap = dict()
+    for line in open('../input/poly_truth.tsv'):
+        lineCount2 += 1
+        if lineCount2 == 1:
+            continue
+        data = line.split('\t')
+        truthMap[data[0]] = int(data[1])
+    for line in open('../input/quant.tsv'):
+        lineCount3 += 1
+        if lineCount3 == 1:
+            continue
+        data1 = line.split('\t')
+        value = data1[4]
+        numReads = float(value[:-1])
+        quantMap[data1[0]] = numReads
+
+    resultMap = dict()
+    for x in truthMap.keys():
+        truthValue = truthMap.get(x)
+        quantValue = quantMap.get(x)
+        if (truthValue is None or quantValue is None):
+            continue
+        errorFraction = (truthValue - quantValue) / truthValue
+        resultMap[x] = errorFraction
+    df = pd.DataFrame(list(resultMap.items()), columns=['TranscriptID', 'ErrorFraction'])
+    df.to_csv("errorFraction_all.csv", encoding='utf-8')
+
+get_FaultyTranscriptError_CSV()
+get_AllTrancriptsError_CSV()
 
 
